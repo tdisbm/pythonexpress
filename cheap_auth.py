42613e5b0e4c7f5cfb57b306f990c58ccc55ca4b
dtufar <dtufar@inthergroup.com>
2021-04-20 20:20:25 +0300

    day 2: exercises

---
+++ cheap_auth.py
@@ -0,0 +1,10 @@
+username = "developer1"
+password = "hackme"
+
+print("Description: This program check if username and password are correct\n")
+
+input_username = input("Please enter username: ")
+input_password = input("Please enter password: ")
+
+print(username == input_username)
+print(password == input_password)
